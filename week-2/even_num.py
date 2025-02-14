146b6eb4513eae40035a401800c5942818cf280d
HavenGo <havengoitom2@gmail.com>
2025-02-14 12:55:15 +0300

    Update even_num.py

--- week-2/even_num.py
+++ week-2/even_num.py
@@ -1,8 +1,6 @@
-list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 ,15, 16, 17, 18, 19, 20]
-
 even_list = []
-for i in list:
+for i in range(1, 21):
     if i % 2 == 0:
         even_list.append(i)
 
-print(f"The even numbers in the list are {even_list}.")
+print(even_list)
